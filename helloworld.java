2db4852abf2b03e5bf72cff059366597c004f7f4
Suneetha-26 <nagasuneetha.kotha@vassarlabs.com>
2024-07-23 14:23:26 +0530

    test

---
+++ helloworld.java
@@ -0,0 +1,12 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Try programiz.pro");
+        File directory = new File("/home/vassar/Downloads/pscene/25bcf52e-afe9-4971-9977-2923b4ebea4f/20220619/fcc/");
+        if (directory.isDirectory()) {
+        	System.out.println("yes");
+        }
+    }
+}
